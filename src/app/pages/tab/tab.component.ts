ae6f6419668fc9c8a4c0d911705ce04ef693ea58
nsreed <norman@normanreed.com>
2022-09-06 13:12:13 -0400

    work toward displaying channels on tab page

--- src/app/pages/tab/tab.component.ts
+++ src/app/pages/tab/tab.component.ts
@@ -1,21 +1,22 @@
 import { Component } from '@angular/core';
 import { BaseAbstract } from '@core/abstract/base.abstract';
 import { GunService } from '../../shared/services/gun.service';
+import { Channel } from '../../models/arena';
 
 @Component({
-    selector: 'app-tab',
-    templateUrl: './tab.component.html',
-    styleUrls: ['./tab.component.scss']
+  selector: 'app-tab',
+  templateUrl: './tab.component.html',
+  styleUrls: ['./tab.component.scss']
 })
 export class TabComponent extends BaseAbstract {
-    constructor(public gunService: GunService) {
-        super();
-        console.log('starting list, getting block from root?');
-        gunService.gun.get('block').on((data)=>{
-            console.log({data});
-        });
-        // gunService.gun.get('block').get('test').put({
-        //     url: 'hello'
-        // });
-    }
+  channels: Channel[] = [
+
+  ];
+  constructor(public gunService: GunService) {
+    super();
+    console.log('starting list, getting block from root?');
+    gunService.gun.get('block').on((data) => {
+      console.log({ data });
+    });
+  }
 }
