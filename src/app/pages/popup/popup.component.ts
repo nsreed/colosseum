9f856f9d0e9010f4693af782c0181ba7669b1529
nsreed <norman@normanreed.com>
2022-09-07 21:08:22 -0400

    work toward creating a complete block record

--- src/app/pages/popup/popup.component.ts
+++ src/app/pages/popup/popup.component.ts
@@ -1,24 +1,44 @@
 import { Component } from '@angular/core';
 import { BaseAbstract } from '@core/abstract/base.abstract';
 import { GunService } from '../../shared/services/gun.service';
+import { TimeStamp } from '../../models/aspects';
 
 @Component({
-    selector: 'app-popup',
-    templateUrl: './popup.component.html',
-    styleUrls: ['./popup.component.scss']
+  selector: 'app-popup',
+  templateUrl: './popup.component.html',
+  styleUrls: ['./popup.component.scss']
 })
 export class PopupComponent extends BaseAbstract {
-    constructor(public gunService: GunService) {
-        super();
+  constructor(public gunService: GunService) {
+    super();
+  }
+
+  async addBlock() {
+    const [current] = await chrome.tabs.query({ active: true, currentWindow: true });
+    const url = current.url as string;
+    const title = current.title;
+
+    const owner = {
+      userName: 'user'
+    }
+    const base64 = 'abcdef0123456789';
+    const urlGun = this.gunService.gun.get('blocks').get(url);
+    // TODO url contains path separators (. & /), you should find a better index
+
+    const timeStamp: TimeStamp = {
+      time: Date.now(),
+      timezoneOffset: new Date().getTimezoneOffset()
     }
 
-    async addBlock() {
-        const [current] = await chrome.tabs.query({ active: true, currentWindow: true });
-        const url = current.url as string;
-        // chrome.tabs.captureVisibleTab();
-
-        const urlGun = this.gunService.gun.get('block').get(url);
-        // TODO url contains path separators (. & /), you should find a better index
-        urlGun.get('time').put(Date.now());
-    }
+    const block = {
+      title,
+      titleText: title,
+      url,
+      owner,
+      base64,
+      timeStamp
+    };
+    console.log({ block });
+    urlGun.put(block);
+  }
 }
