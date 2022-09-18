2b224d2afba5ce708b8c5b8db315bf62bb93ffdc
nsreed <norman@normanreed.com>
2022-09-18 19:25:06 -0400

    debugging will forever be impossible, but the browser understands where files are now

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,27 +2,27 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-    {
-        path: 'popup',
-        loadChildren: () => import('./pages/popup/popup.module').then(m => m.PopupModule)
-    },
-    {
-        path: 'tab',
-        loadChildren: () => import('./pages/tab/tab.module').then(m => m.TabModule)
-    },
-    {
-        path: 'options',
-        loadChildren: () => import('./pages/options/options.module').then(m => m.OptionsModule)
-    },
-    {
-        path: '',
-        redirectTo: '/popup',
-        pathMatch: 'full'
-    }
+  {
+    path: 'popup',
+    loadChildren: () => import('./pages/popup/popup.module').then(m => m.PopupModule)
+  },
+  {
+    path: 'tab',
+    loadChildren: () => import('./pages/tab/tab.module').then(m => m.TabModule)
+  },
+  {
+    path: 'options',
+    loadChildren: () => import('./pages/options/options.module').then(m => m.OptionsModule)
+  },
+  {
+    path: '',
+    redirectTo: '/popup',
+    pathMatch: 'full'
+  }
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes, { useHash: true, initialNavigation: 'enabled', relativeLinkResolution: 'legacy' })],
-    exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, { useHash: true, initialNavigation: 'enabledBlocking' })],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
